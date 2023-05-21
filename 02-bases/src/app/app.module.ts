0cfcf0fabcd31106556b390ea84609bf1cfaf228
JesúsMendoza <mendoza.git@gmail.com>
2023-05-21 09:10:03 -0600

    fist module added

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
-    declarations: [AppComponent, CounterComponent, HeroComponent, ListComponent],
-    imports: [BrowserModule],
+    declarations: [AppComponent, HeroComponent, ListComponent],
+    imports: [BrowserModule, CounterModule],
     providers: [],
     bootstrap: [AppComponent],
 })
---
+++ 02-bases/src/app/counter/counter.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { CounterComponent } from './components/counter/counter.component';
+@NgModule({
+    declarations: [CounterComponent],
+    exports: [CounterComponent],
+})
+export class CounterModule {}
