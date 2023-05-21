c09319169a0146cba99b9eb424315e6408e74c5f
JesúsMendoza <mendoza.git@gmail.com>
2023-05-21 13:48:14 -0600

    second module added

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/counter.module';
+import { HeroModule } from './heroes/heroModule';
 
 @NgModule({
-    declarations: [AppComponent, HeroComponent, ListComponent],
-    imports: [BrowserModule, CounterModule],
+    declarations: [AppComponent],
+    imports: [BrowserModule, CounterModule, HeroModule],
     providers: [],
     bootstrap: [AppComponent],
 })
