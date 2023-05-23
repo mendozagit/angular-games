7c67abfcf8749feaef7bf6fd505162fcf6131443
JesúsMendoza <mendoza.git@gmail.com>
2023-05-23 17:15:45 -0600

    dbz module added

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main.page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 @NgModule({
-    declarations: [MainPageComponent],
+    declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
     exports: [MainPageComponent],
     imports: [CommonModule],
 })
