a84fd922cc31538f9d6dd4904dfe6a0c6c673692
JesúsMendoza <mendoza.git@gmail.com>
2023-05-23 17:33:32 -0600

    angular input directive -added

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/Character.Interface';
 
 @Component({
     selector: 'bdz-list',
     templateUrl: './list.component.html',
     styleUrls: ['./list.component.css'],
 })
-export class ListComponent {}
+export class ListComponent {
+    @Input()
+    public characterList: Character[] = [
+        {
+            name: 'Trunk',
+            power: 10,
+        },
+    ];
+}
--- 02-bases/src/app/dbz/pages/main.page.component.ts
+++ 02-bases/src/app/dbz/pages/main.page.component.ts
@@ -1,7 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/Character.Interface';
 
 @Component({
     selector: 'app-dbz-main-page',
     templateUrl: 'main.page.component.html',
 })
-export class MainPageComponent {}
+export class MainPageComponent {
+    public charaters: Character[] = [
+        {
+            name: 'Klilin',
+            power: 1000,
+        },
+        {
+            name: 'Goku',
+            power: 9500,
+        },
+        {
+            name: 'Vegeta',
+            power: 7500,
+        },
+    ];
+}
