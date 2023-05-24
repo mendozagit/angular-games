c1dc3f4a5f19ec57a6ba1e99ee17b0e94939384b
JesúsMendoza <mendoza.git@gmail.com>
2023-05-23 19:17:48 -0600

    angular ngModel-added

--- 02-bases/src/app/dbz/components/add-character/add-character.component.ts
+++ 02-bases/src/app/dbz/components/add-character/add-character.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/Character.Interface';
 
 @Component({
     selector: 'dbz-add-character',
     templateUrl: './add-character.component.html',
     styleUrls: ['./add-character.component.css'],
 })
-export class AddCharacterComponent {}
+export class AddCharacterComponent {
+    public character: Character = {
+        name: 'ABC',
+        power: 5,
+    };
+
+    /**
+     * saveCharacter
+     */
+    public saveCharacter(): void {
+        console.log(this.character);
+    }
+}
--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { MainPageComponent } from './pages/main.page.component';
 import { ListComponent } from './components/list/list.component';
 import { AddCharacterComponent } from './components/add-character/add-character.component';
@@ -7,6 +9,6 @@ import { AddCharacterComponent } from './components/add-character/add-character.
 @NgModule({
     declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
     exports: [MainPageComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
 })
 export class DbzModule {}
