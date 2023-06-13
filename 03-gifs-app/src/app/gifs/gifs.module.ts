4aaefc0c2bd42df8fdf8c0a2d4c67c452bfc7075
JesúsMendoza <mendoza.git@gmail.com>
2023-06-13 17:06:11 -0600

    card-list component added

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box.component';
+import { CardListComponent } from './components/card-list/card-list/card-list.component';
 
 @NgModule({
-    declarations: [HomePageComponent, SearchBoxComponent],
+    declarations: [HomePageComponent, SearchBoxComponent, CardListComponent],
     imports: [CommonModule],
     exports: [HomePageComponent],
 })
