57418afa84b2de2d1b31d47f9a09d5dbe721c900
JesúsMendoza <mendoza.git@gmail.com>
2023-06-14 12:07:46 -0600

    view child added

--- 03-gifs-app/src/app/gifs/components/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'gifs-search-box',
@@ -10,9 +10,27 @@ import { Component } from '@angular/core';
             placeholder="Buscar gifs"
             name=""
             id=""
+            (keyup.enter)="searchTag()"
+            #searchInput
         />
     `,
 })
 export class SearchBoxComponent {
     constructor() {}
+
+    @ViewChild('searchInput')
+    public searchInput!: ElementRef<HTMLInputElement>;
+    /**
+     * searchTag
+     */
+    // public searchTag(tag: string) {
+    //     console.log(tag);
+    // }
+
+    public searchTag() {
+        //     console.log(tag);
+        const searchText = this.searchInput.nativeElement.value;
+
+        console.log(searchText);
+    }
 }
