b3725acd3313c01364541c6d79e5c6401c45b065
JesúsMendoza <mendoza.git@gmail.com>
2023-06-29 13:44:24 -0600

    card component added

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { SearchBoxComponent } from './components/search-box.component';
 import { CardListComponent } from './components/card-list/card-list/card-list.component';
 import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchBoxComponent } from './components/search-box.component';
 
 @NgModule({
-    declarations: [HomePageComponent, SearchBoxComponent, CardListComponent, GifsCardComponent],
-    imports: [CommonModule],
+    declarations: [
+        CardListComponent,
+        GifsCardComponent,
+        HomePageComponent,
+        SearchBoxComponent,
+    ],
+    imports: [CommonModule, SharedModule],
     exports: [HomePageComponent],
 })
 export class GifsModule {}
--- 03-gifs-app/src/app/shared/shared.module.ts
+++ 03-gifs-app/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { CardListComponent } from './components/card-list/card-list/card-list.component';
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
 @NgModule({
-    declarations: [SidebarComponent, CardListComponent],
+    declarations: [SidebarComponent, CardListComponent, LazyImageComponent],
     imports: [CommonModule],
-    exports: [SidebarComponent],
+    exports: [SidebarComponent, LazyImageComponent],
 })
 export class SharedModule {}
