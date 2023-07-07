589e6d15eb134bc5c2b95a1fabfafb2664004030
JesúsMendoza <mendoza.git@gmail.com>
2023-07-06 19:14:08 -0600

    by region page optimized

--- 04-country-app/src/app/modules/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-country-app/src/app/modules/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,21 +2,32 @@ import { Component } from '@angular/core';
 import { CountryService } from '../../services/conuntry.service';
 import { Country } from '../../interfaces/Country';
 
+type Region = 'Africa' | 'America' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
     selector: 'countries-by-region-page',
     templateUrl: './by-region-page.component.html',
     styles: [],
 })
 export class ByRegionPageComponent {
+    public selectedRegion?: Region;
     public countries: Country[] = [];
+    public regions: Region[] = [
+        'Africa',
+        'America',
+        'Asia',
+        'Europe',
+        'Oceania',
+    ];
 
     constructor(private countryService: CountryService) {}
 
-    searchByRegion(textToSearch: string): void {
-        this.countryService.searchRegion(textToSearch).subscribe((x) => {
+    searchByRegion(region: Region): void {
+        this.countryService.searchRegion(region).subscribe((x) => {
             this.countries = x;
         });
+        this.selectedRegion = region;
 
-        console.log(textToSearch + ' From searchByRegion');
+        console.log(region + ' From searchByRegion');
     }
 }
