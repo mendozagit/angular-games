20bc020c72cd22c3a9a2b5715653e0c2103e3242
JesúsMendoza <mendoza.git@gmail.com>
2023-07-13 19:42:17 -0600

    date pipes added

--- 05-pipes-app/src/app/app.module.ts
+++ 05-pipes-app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgPrimeModule } from './ng-prime/ng-prime.module';
 import { SharedModule } from './shared/shared.module';
+
+//internacionalization settings
+import localEsMX from '@angular/common/locales/es-MX';
+import localFrCA from '@angular/common/locales/fr-CA';
+import localDeCH from '@angular/common/locales/de-CH';
+
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localEsMX);
+registerLocaleData(localFrCA);
+registerLocaleData(localDeCH);
 
 @NgModule({
     declarations: [AppComponent],
@@ -16,7 +26,7 @@ import { SharedModule } from './shared/shared.module';
         NgPrimeModule,
         SharedModule,
     ],
-    providers: [],
+    providers: [{ provide: LOCALE_ID, useValue: 'es-MX' }],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
