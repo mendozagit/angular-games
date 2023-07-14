902902e73cfe67d104ca368521e1ddb61e3d0cbb
JesúsMendoza <mendoza.git@gmail.com>
2023-07-13 20:55:35 -0600

    decimal pipes added

--- 05-pipes-app/src/app/products/pages/numberspage/numberspage.component.ts
+++ 05-pipes-app/src/app/products/pages/numberspage/numberspage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-numberspage',
-  templateUrl: './numberspage.component.html',
-  styleUrls: ['./numberspage.component.css']
+    selector: 'app-numberspage',
+    templateUrl: './numberspage.component.html',
+    styleUrls: ['./numberspage.component.css'],
 })
 export class NumberspageComponent {
-
+    public totalSells: number = 256789.5698586;
+    public percent: number = 0.78598;
 }
