13ea0b77589450e687dd4d9d72ebcebc34e87c63
JesúsMendoza <mendoza.git@gmail.com>
2023-07-14 10:00:44 -0600

    i18nselect pipe added

--- 05-pipes-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipes-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-uncommon-page',
-  templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+    selector: 'app-uncommon-page',
+    templateUrl: './uncommon-page.component.html',
+    styleUrls: ['./uncommon-page.component.css'],
 })
 export class UncommonPageComponent {
+    public name: string = 'Jesus';
+    public gender: 'male' | 'female' = 'male';
+    public invitationMap = {
+        male: 'invitarlo',
+        female: 'invitarla',
+    };
 
+    public changeClient(): void {
+        if (this.name === 'Jesus') {
+            this.name = 'Leticia';
+            this.gender = 'female';
+        } else {
+            this.name = 'Jesus';
+            this.gender = 'male';
+        }
+    }
 }
