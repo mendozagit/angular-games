0e5157d723edf5278d09ea2d27c136f2669cfe88
JesúsMendoza <mendoza.git@gmail.com>
2023-07-16 17:38:02 -0600

    layout pages added

--- 06-herosApp/src/app/auth/auth-routing.module.ts
+++ 06-herosApp/src/app/auth/auth-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutAuthPageComponent } from './pages/layout-page/layout-auth-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { Error404PageComponent } from '../shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [{ path: '', component: LayoutAuthPageComponent }];
+const routes: Routes = [
+    {
+        path: '',
+        component: LayoutAuthPageComponent,
+        children: [
+            { path: 'login', component: LoginPageComponent },
+            { path: 'register', component: RegisterPageComponent },
+            { path: '', redirectTo: 'login', pathMatch: 'full' },
+            { path: '**', component: Error404PageComponent },
+        ],
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
--- 06-herosApp/src/app/heroes/heroes-routing.module.ts
+++ 06-herosApp/src/app/heroes/heroes-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutHeroPageComponent } from './pages/layout-page/layout-hero-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
-const routes: Routes = [{ path: '', component: LayoutHeroPageComponent }];
+const routes: Routes = [
+    {
+        path: '',
+        component: LayoutHeroPageComponent,
+        children: [
+            { path: 'new-hero', component: NewPageComponent },
+            { path: 'search', component: SearchPageComponent },
+            { path: 'edit/:id', component: NewPageComponent },
+            { path: 'list', component: ListPageComponent },
+            { path: ':id', component: HeroPageComponent },
+            { path: '**', redirectTo: 'list' },
+        ],
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
