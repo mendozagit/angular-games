4ec4e730c68ad3a90e290174e897ed76b7ba353d
JesúsMendoza <mendoza.git@gmail.com>
2023-07-17 16:22:56 -0600

    heros layout added

--- 06-herosApp/src/app/heroes/heroes.module.ts
+++ 06-herosApp/src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { NewPageComponent } from './pages/new-page/new-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutHeroPageComponent } from './pages/layout-page/layout-hero-page.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
     declarations: [
@@ -16,6 +17,6 @@ import { LayoutHeroPageComponent } from './pages/layout-page/layout-hero-page.co
         HeroPageComponent,
         LayoutHeroPageComponent,
     ],
-    imports: [CommonModule, HeroesRoutingModule],
+    imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
 export class HeroesModule {}
--- 06-herosApp/src/app/heroes/pages/layout-page/layout-hero-page.component.ts
+++ 06-herosApp/src/app/heroes/pages/layout-page/layout-hero-page.component.ts
@@ -5,4 +5,22 @@ import { Component } from '@angular/core';
     templateUrl: './layout-hero-page.component.html',
     styles: [],
 })
-export class LayoutHeroPageComponent {}
+export class LayoutHeroPageComponent {
+    public menuItems = [
+        {
+            label: 'List',
+            icon: 'label',
+            url: './list',
+        },
+        {
+            label: 'Add',
+            icon: 'add',
+            url: './new-hero',
+        },
+        {
+            label: 'Search',
+            icon: 'search',
+            url: './search',
+        },
+    ];
+}
