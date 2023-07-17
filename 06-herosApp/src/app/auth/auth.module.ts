9b65293534bb5eabb5d30b4f20f6468eaf934b34
JesúsMendoza <mendoza.git@gmail.com>
2023-07-17 15:27:29 -0600

    material imported to auth model

--- 06-herosApp/src/app/auth/auth.module.ts
+++ 06-herosApp/src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { LayoutAuthPageComponent } from './pages/layout-page/layout-auth-page.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
     declarations: [
@@ -12,6 +13,6 @@ import { LayoutAuthPageComponent } from './pages/layout-page/layout-auth-page.co
         LoginPageComponent,
         RegisterPageComponent,
     ],
-    imports: [CommonModule, AuthRoutingModule],
+    imports: [CommonModule, AuthRoutingModule, MaterialModule],
 })
 export class AuthModule {}
