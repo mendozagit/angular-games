6ecb53d8b8aa015b9f9fc39606b7fc0822572a48
JesúsMendoza <mendoza.git@gmail.com>
2023-07-18 17:36:43 -0600

    assets added

---
+++ 06-herosApp/src/app/heroes/pipes/hero-image-pipe.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Hero } from '../interfaces/hero.interface';
+
+@Pipe({
+    name: 'heroimage',
+})
+export class HeroImagePipe implements PipeTransform {
+    transform(hero: Hero): string {
+        if (!hero.id && hero.alt_image) {
+            return 'assets/no-image.jpg';
+        }
+
+        if (hero.alt_image) return hero.alt_image;
+
+        const src = `assets/heroes/${hero.id}.jpg`;
+
+        return src;
+    }
+}
