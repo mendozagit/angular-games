3988ca3915f0e2ce081b55e6835c3343d5e27067
JesúsMendoza <mendoza.git@gmail.com>
2023-07-21 17:34:57 -0600

    search hero added

--- 06-herosApp/src/app/heroes/pages/search-page/search-page.component.ts
+++ 06-herosApp/src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,5 +1,8 @@
+import { HeroService } from './../../services/heroes.service';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { map } from 'rxjs';
 
 @Component({
     selector: 'app-search-page',
@@ -7,5 +10,18 @@ import { FormControl } from '@angular/forms';
     styles: [],
 })
 export class SearchPageComponent {
-    public inputSearch = new FormControl('');
+    public searchInput = new FormControl('');
+    public heros: Hero[] = [];
+    public searchText: string = '';
+
+    constructor(private heroService: HeroService) {}
+    searchHero() {
+        this.searchText = this.searchInput.value?.trim() || '';
+
+        this.heroService
+            .getSuggestions(this.searchText)
+            .subscribe((heros) => (this.heros = heros));
+
+        console.log(this.searchText);
+    }
 }
