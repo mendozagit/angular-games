9ca6c7cafe042b138bd4c211e00b0b72c5d2eaba
JesúsMendoza <mendoza.git@gmail.com>
2023-07-23 13:02:21 -0600

    hero crud hero service added

--- 06-herosApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-herosApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HeroService } from '../../services/heroes.service';
 
 @Component({
     selector: 'app-new-page',
@@ -8,6 +10,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
     styles: [],
 })
 export class NewPageComponent {
+    constructor(private router: Router, private heroService: HeroService) {}
     public hero?: Hero;
     public heroForm = new FormGroup({
         id: new FormControl(''),
@@ -29,4 +32,31 @@ export class NewPageComponent {
             desc: 'Marvel - Comics',
         },
     ];
+
+    onSubmit(): void {
+        if (!this.heroForm.valid) return;
+
+        this.hero = this.heroForm.value as Hero;
+
+        if (this.hero?.id) {
+            // update
+            this.heroService.updateHero(this.hero).subscribe((response) => {
+                // show confirmation message
+            });
+            return;
+        }
+
+        // Insert
+        this.heroService.addHero(this.hero).subscribe((response) => {
+            // show confirmation message and redirect  /heros/edit/hero.id
+        });
+
+        console.log({
+            valid: this.heroForm.valid,
+            form: this.heroForm,
+            getRawValue: this.heroForm.getRawValue,
+        });
+
+        //this.router.navigateByUrl('heroes/list');
+    }
 }
