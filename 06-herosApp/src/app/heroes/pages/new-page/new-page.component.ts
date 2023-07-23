0cdc1492fa78db9c2002bae1f2eb1e669c646733
JesúsMendoza <mendoza.git@gmail.com>
2023-07-22 19:51:20 -0600

    add hero added

--- 06-herosApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-herosApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
 
 @Component({
-  selector: 'app-new-page',
-  templateUrl: './new-page.component.html',
-  styles: [
-  ]
+    selector: 'app-new-page',
+    templateUrl: './new-page.component.html',
+    styles: [],
 })
 export class NewPageComponent {
-
+    public hero?: Hero;
+    public publishers = [
+        {
+            id: 'DC Comics',
+            desc: 'DC - Comics',
+        },
+        {
+            id: 'Marvel Comics',
+            desc: 'Marvel - Comics',
+        },
+    ];
 }
