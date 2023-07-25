00aab101f972b092704425652b9b5754f4c742f5
JesúsMendoza <mendoza.git@gmail.com>
2023-07-25 14:26:20 -0600

    basic reactive form added

--- 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  templateUrl: './basic-page.component.html',
-  styles: [
-  ]
+    templateUrl: './basic-page.component.html',
+    styles: [],
 })
 export class BasicPageComponent {
+    constructor(private formBuilder: FormBuilder) {}
 
+    // public form = new FormGroup({
+    //     name: new FormControl(''),
+    //     price: new FormControl(0),
+    //     inStorage: new FormControl(0),
+    // });
+
+    public form: FormGroup = this.formBuilder.group({
+        name: [''],
+        price: [0],
+        inStorage: [0],
+    });
+
+    /**
+     * onSubmit
+     */
+    public onSubmit() {
+        console.log('form', this.form);
+    }
 }
--- 07-formsApp/src/app/reactive/reactive.module.ts
+++ 07-formsApp/src/app/reactive/reactive.module.ts
@@ -5,6 +5,7 @@ import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -12,6 +13,6 @@ import { SwitchesPageComponent } from './pages/switches-page/switches-page.compo
         DynamicPageComponent,
         SwitchesPageComponent,
     ],
-    imports: [CommonModule, ReactiveRoutingModule],
+    imports: [CommonModule, ReactiveRoutingModule, ReactiveFormsModule],
 })
 export class ReactiveModule {}
