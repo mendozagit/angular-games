9b0ec7b776edf77a5727343274d8858fcc2c793f
JesúsMendoza <mendoza.git@gmail.com>
2023-07-25 16:34:46 -0600

    dynamic reactive form added

--- 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
-  templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
+    templateUrl: './dynamic-page.component.html',
+    styles: [],
 })
 export class DynamicPageComponent {
+    /**
+     *
+     */
+    constructor(private formBuilder: FormBuilder) {}
+    public form: FormGroup = this.formBuilder.group({
+        name: ['', [Validators.required, Validators.maxLength(3)]],
+        favoriteGames: this.formBuilder.array([
+            ['Metal Gear', Validators.required],
+            ['Death Strading', Validators.required],
+        ]),
+    });
 
+    onSubmit() {
+        if (this.form.invalid) {
+            this.form.markAllAsTouched();
+            return;
+        }
+
+        console.log(this.form);
+
+        this.form.reset();
+    }
+
+    get favoriteGames() {
+        return (this.form.get('favoriteGames') as FormArray).controls;
+    }
 }
