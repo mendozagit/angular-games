063c8d279a985cbf7eaf6d08fdadb7d616e8157b
JesúsMendoza <mendoza.git@gmail.com>
2023-07-24 18:25:35 -0600

    routes added

--- 07-formsApp/src/app/auth/auth-routing.module.ts
+++ 07-formsApp/src/app/auth/auth-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        children: [
+            { path: 'register', component: RegisterPageComponent },
+            { path: '**', redirectTo: 'register' },
+        ],
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
