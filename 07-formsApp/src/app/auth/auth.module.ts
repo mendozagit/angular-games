e85b5f32ed4d8a2ea0b0c8fe5f6235ba29725a0c
JesúsMendoza <mendoza.git@gmail.com>
2023-07-24 18:06:39 -0600

    reactive forms app added

--- 07-formsApp/src/app/auth/auth.module.ts
+++ 07-formsApp/src/app/auth/auth.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+    declarations: [
+    RegisterPageComponent
+  ],
+    imports: [CommonModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}
