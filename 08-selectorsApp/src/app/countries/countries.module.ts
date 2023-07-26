6c5a91f8386737fa5959f32e43ea51a60aa015f9
JesúsMendoza <mendoza.git@gmail.com>
2023-07-25 19:00:32 -0600

    selector app added

---
+++ 08-selectorsApp/src/app/countries/countries.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+
+@NgModule({
+    declarations: [
+    SelectorPageComponent
+  ],
+    imports: [CommonModule],
+})
+export class CountriesModule {}
---
+++ 08-selectorsApp/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent {
+
+}
