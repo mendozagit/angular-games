93cc5453bb1494f5b1a9f5a4860975fa7dca592b
JesúsMendoza <mendoza.git@gmail.com>
2023-07-26 10:49:29 -0600

    country service added

--- 08-selectorsApp/src/app/countries/services/country.service.ts
+++ 08-selectorsApp/src/app/countries/services/country.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Region } from '../interfaces/country.interface';
 
 @Injectable({ providedIn: 'root' })
 export class CountryService {
     constructor(private httpClient: HttpClient) {}
+
+    private _regions: Region[] = [
+        Region.Africa,
+        Region.Americas,
+        Region.Asia,
+        Region.Europe,
+        Region.Oceania,
+    ];
+
+    get regions(): Region[] {
+        return [...this._regions];
+    }
 }
