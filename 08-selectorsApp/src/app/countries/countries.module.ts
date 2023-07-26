ab37bf7a9a94013b702fd968e4f42f1f8fa37c0c
JesúsMendoza <mendoza.git@gmail.com>
2023-07-26 09:31:57 -0600

    app 08 initial configs added

--- 08-selectorsApp/src/app/countries/countries.module.ts
+++ 08-selectorsApp/src/app/countries/countries.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CountriesRoutingModule } from './countries-routing.module';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    CountriesRoutingModule
-  ]
+    declarations: [SelectorPageComponent],
+    imports: [CommonModule, CountriesRoutingModule, ReactiveFormsModule],
 })
-export class CountriesModule { }
+export class CountriesModule {}
--- 08-selectorsApp/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 08-selectorsApp/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-selector-page',
-  templateUrl: './selector-page.component.html',
-  styles: [
-  ]
+    selector: 'app-selector-page',
+    templateUrl: './selector-page.component.html',
+    styles: [],
 })
 export class SelectorPageComponent {
+    constructor(private formBuilder: FormBuilder) {}
 
+    public form: FormGroup = this.formBuilder.group({
+        region: ['', [Validators.required]],
+        country: ['', [Validators.required]],
+        borders: ['', [Validators.required]],
+    });
 }
