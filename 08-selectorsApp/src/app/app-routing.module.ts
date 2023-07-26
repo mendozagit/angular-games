9e236f0775aac8016ddfd9dc7e548d5481445792
JesúsMendoza <mendoza.git@gmail.com>
2023-07-26 09:05:26 -0600

    app 08 initial configs added

--- 08-selectorsApp/src/app/app-routing.module.ts
+++ 08-selectorsApp/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'countries',
+        loadChildren: () =>
+            import('./countries/countries.module').then(
+                (m) => m.CountriesModule
+            ),
+    },
+    {
+        path: '**',
+        redirectTo: 'countries',
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- 08-selectorsApp/src/app/countries/countries.module.ts
+++ 08-selectorsApp/src/app/countries/countries.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+
+import { CountriesRoutingModule } from './countries-routing.module';
+
 
 @NgModule({
-    declarations: [
-    SelectorPageComponent
-  ],
-    imports: [CommonModule],
+  declarations: [],
+  imports: [
+    CommonModule,
+    CountriesRoutingModule
+  ]
 })
-export class CountriesModule {}
+export class CountriesModule { }
