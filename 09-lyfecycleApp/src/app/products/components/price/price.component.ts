f9bc8155e0f97c6a752e578e362f252c7cb7c399
JesúsMendoza <mendoza.git@gmail.com>
2023-07-27 18:33:05 -0600

    lifecycle app added

---
+++ 09-lyfecycleApp/src/app/products/components/price/price.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'products-price',
+    templateUrl: './price.component.html',
+    styleUrls: ['./price.component.css'],
+})
+export class PriceComponent {}
